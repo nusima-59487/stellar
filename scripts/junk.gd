3a92fb8d640e316ff1b7e4cba38fec24a35d78f9
soulmars432-ctrl <soulmars432@gmail.com>
2026-06-05 19:10:51 -0700

    orbit

--- scripts/junk.gd
+++ scripts/junk.gd
@@ -1,18 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-
+const C_SPEED = 100.0
+const T_SPEED = 200.0
 
 func _physics_process(_shit) -> void:
 	var target_pos := Vector2.ZERO; 
 	var my_pos = self.position; 
 	var direction = target_pos - my_pos
-
 	# Calculate angle to target
 	var angle_to_target := atan2(direction.y, direction.x)
+	var perpendicular_angle := atan2(direction.y, direction.x) + PI/2
 	# Set velocity based on angle
-	velocity.x = SPEED * cos(angle_to_target)
-	velocity.y = SPEED * sin(angle_to_target)
+	
+	velocity.x = C_SPEED * cos(angle_to_target) + T_SPEED * cos(perpendicular_angle)
+	velocity.y = C_SPEED * sin(angle_to_target) + T_SPEED * sin(perpendicular_angle)
 
 	move_and_slide()
