04e80461863d076051a1f8119df7795798152e17
soulmars432-ctrl <soulmars432@gmail.com>
2026-06-05 21:03:22 -0700

    hgh

--- scripts/junk.gd
+++ scripts/junk.gd
@@ -10,6 +10,7 @@ var all_stage = ["big","med","smol"]
 @export var T_SPEED = 200.0
 
 func _physics_process(_shit) -> void:
+	$AnimatedSprite2D.animation = all_stage[stage]
 	var target_pos := Vector2.ZERO; 
 	var my_pos = self.position; 
 	var direction = target_pos - my_pos
@@ -27,15 +28,22 @@ func _physics_process(_shit) -> void:
 func take_damage(amount: int) -> void:
 	health -= amount
 	if health <= 0:
+		break_junk()
+		health = 10
+	if health <= 0 && stage ==2:
 		queue_free()
-
 
 func break_junk() -> void:
 	var JUNK = preload("res://junk.tscn")
 	var junk_instance = JUNK.instantiate()
 	junk_instance.position = self.position
 	junk_instance.rotation = self.rotation - PI/2
+	if junk_instance.stage + 1 < 3:
+		junk_instance.stage += 1
+	if stage + 1 < 3:
+		stage += 1
 	add_sibling(junk_instance)
+	
 
 
 
