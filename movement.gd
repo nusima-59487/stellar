b54dc7ace96379057b7a77912b81f856615cb6af
soulmars432-ctrl <soulmars432@gmail.com>
2026-06-05 18:44:18 -0700

    added player movement

---
+++ movement.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 10.0
+
+
+func _physics_process(delta: float) -> void:
+	
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	if direction_x:
+		velocity.x += direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED/3)
+	if direction_y:
+		velocity.y += direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED/3)
+		
+	look_at(get_global_mouse_position())
+
+	move_and_slide()
