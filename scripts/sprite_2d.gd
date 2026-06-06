5d85b3045846d19d982436c3c651b89908351002
soulmars432-ctrl <soulmars432@gmail.com>
2026-06-06 13:45:30 -0700

    arrow

---
+++ scripts/sprite_2d.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+@onready var player: CharacterBody2D = $"../../../Player"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var target_angle = atan2(-player.position.y, -player.position.x)
+	rotation = target_angle + PI/2
